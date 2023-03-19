87ac6cfaf4049eea01025e45e596de3787eeb1ec
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-19 15:03:27 -0700

    al and alx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main -  entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	 printf("%lu \n"sizeof(char));
+	 printf("%lu \n"sizeof(int));
+	 printf("%lu \n"sizeof(long int));
+	 printf("%lu \n"sizeof(long long int));
+	 printf("%lu \n"sizeof(float));
+return (0);
+}
