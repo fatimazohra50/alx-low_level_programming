90576c9fdc5f9dc29704d4ad0c98e8ea13d8ce5b
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-19 12:38:24 -0700

    alx alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
  * main -Entry point
- *
- * Dascription: A C program that prints with put function 
- *
- * Return:Alawys 0 (Success)
+ * Return: Alawys 0 (Success)
 */
-int main (void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
