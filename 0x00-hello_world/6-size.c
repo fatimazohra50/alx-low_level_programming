a32bb6ac6ba1b1ed3a926c3fa6fbd61f10810bb9
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-19 15:37:26 -0700

    alx and al

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main -  entry point
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	 printf("%lu \n"sizeof(char));
-	 printf("%lu \n"sizeof(int));
-	 printf("%lu \n"sizeof(long int));
-	 printf("%lu \n"sizeof(long long int));
-	 printf("%lu \n"sizeof(float));
+	 printf("%u \n"sizeof(char));
+	 printf("%u \n"sizeof(int));
+	 printf("%u \n"sizeof(long int));
+	 printf("%u \n"sizeof(long long int));
+	 printf("%u \n"sizeof(float));
 return (0);
 }
