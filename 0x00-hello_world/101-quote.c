cb8609a2146ebbb45d1222f5f21c1c79f703e55f
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-19 17:10:06 -0700

    aldh alx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ *main - Entry point
+ *Description :print a qoute using write function
+ * ssize_t write(int fd .const void *buf.size_t count ) :
+ *Return 1 (Success)
  */
-int main (void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+int main(void)
+{char qui[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 return (1);
 }
 
