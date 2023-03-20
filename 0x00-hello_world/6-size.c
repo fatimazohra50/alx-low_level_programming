8a2e427ba4bd93be4be6485ac348eae2d09d780b
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-20 11:55:31 -0700

    alx alx alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	 printf("%zu \n", sizeof(char));
-	 printf("%zu \n", sizeof(int));
-	 printf("%zu \n", sizeof(long int));
-	 printf("%zu \n", sizeof(long long int));
-	 printf("%zu \n", sizeof(float));
+	 printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	 printf("Size of an int:%zu byte(s)\n", sizeof(int));
+	 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
