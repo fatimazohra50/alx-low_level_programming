ecd1b75ab9e28f42d1e1327c56b432aeac84e6d1
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-27 19:59:51 -0700

    101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -generates random passwords for 101-ctackme
+ * Return::zero
+ */
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+	}
+	putchar(2772 - sum);
+	return (0);
+}
