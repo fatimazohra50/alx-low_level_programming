62e12993c998a459ae330db9186905c627afba62
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-03-30 18:25:39 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- * _strncat - a function that concatenates two strings.
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most number of bytes from @src
- * Return : @dest
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
