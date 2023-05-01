24aa6107dc3a119e348ee8731f337b87120ee789
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-01 15:20:07 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,12 +8,12 @@
  */
 char *_strdup(char *str)
 {
-	int i = 0, size = 0;
+	int i, size = 0;
 	char *m;
 
 	if (str == NULL)
 		return (NULL);
-	for (; str[size] != '\n'; size++)
+	for (; str[size] != '\0'; size++)
 		m = malloc(size * sizeof(*str) + 1);
 	if (m == 0)
 		return (NULL);
