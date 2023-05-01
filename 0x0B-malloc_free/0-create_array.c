046cde1a0b7285b565bb66e6d7a5c0a228a3e373
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-01 14:54:02 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *create_array - create an array of size size assign char,
+ * *create_array - creates an array of chars.
+ * and initializes it with a specific char.
  * @size: size of the array
- * @c: char to assign
- * Descriotion: creat array of size and assign char c
- * Return : pointer to the array, or NULL if fail
+ * @c: char to initialize
+ * Return: pointer to the array initialized or NULL
  */
 char *create_array(unsigned int size, char c)
 {
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\n'; size++)
+		m = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+		return (NULL);
+	for (; i < size; i++)
+		m[i] = str[i];
+	return (m);
+}
