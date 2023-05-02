dbfae85c9179db46018c0b61b81a4c65b1cf1bb3
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-02 09:26:18 -0700

    I will show you how great I am

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all the arguments of your program.
+ * @ac: input
+ * @av: array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int wrdcnt(char *s)
+{
+        int i, n = 0;
+
+        for (i = 0; s[i]; i++)
+        {
+                if (s[i] == ' ')
+                {
+                        if (s[i + 1] != ' ' && s[i + 1] != '\0')
+                                n++;
+                }
+                else if (i == 0)
+                        n++;
+        }
+        n++;
+        return (n);
+}
+/**
+ * **strtow -  splits a string into words.
+ * @str: string
+ * Return: array
+ */
+char **strtow(char *str)
+{
+	int i, q, k, w, n = 0, c = 0;
+	char **a;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	n = wrdcnt(str);
+	if (n == 1)
+		return (NULL);
+	a = (char **)malloc(n * sizeof(char *));
+	if (a == NULL)
+		return (NULL);
+	a[n - 1] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0  || str[i - 1] == ' '))
+		{
+			for (q = 1; str[i + q] != ' ' && str[i + q]; q++)
+				;
+			q++;
+			a[c] = (char *)malloc(q * sizeof(char));
+			q--;
+			if (a[c] == NULL)
+			{
+				for (k = 0; k < c; k++)
+					free(a[k]);
+				free(a[n - 1]);
+				free(a);
+				return (NULL);
+			}
+			for (w = 0; w < q; w++)
+				a[c][w] = str[i + w];
+			a[c][w] = '\0';
+			c++;
+			i += q;
+		}
+		else
+			i++;
+	}
+	return (a);
+}
+
