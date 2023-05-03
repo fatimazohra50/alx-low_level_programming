4a241575566ea31a81ead21ce098603e7081cd45
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-02 19:59:22 -0700

    9-main.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *te;p = *head;
+listint_t *te; p = *head;
 listint_t *current = NULL;
 unsigned int i = 0;
 
@@ -24,11 +24,11 @@ free(temp);
 return (1);
 }
 
-while (i < index -1)
+while (i < index - 1)
 {
-if (!temp || ! (temp->next))
+if (!temp || !(temp->next))
 return (-1);
-temp =temp->next;
+temp = temp->next;
 i++;
 }
 
