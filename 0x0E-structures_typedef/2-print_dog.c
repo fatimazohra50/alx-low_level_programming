a7fa0f583906f77ff6c6447e807b09d2f5128da6
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-03 15:24:44 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,6 @@
  * @d: struct dog to print
  */
 void print_dog(struct dog *d)
-
 {
 if (d == NULL)
 return;
@@ -16,5 +15,5 @@ if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name  d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
