cc155b13fcfe88b92ec43286d607484984af7ae4
Fatimazohra50 <haninfatimazohrgit config --global user.email haninfatimazohra@gmail.com>
2023-05-02 18:55:32 -0700

    head

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -13,8 +13,8 @@ size_t listint_len(const listint_t *h)
 
 	while (h)
 	{
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
  * @idx: index of the list where the new
  * node should be added. Index starts at 0
  * @n: data
- * Returns: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
