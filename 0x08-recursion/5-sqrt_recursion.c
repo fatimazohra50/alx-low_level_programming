74ba09a7e2a66b3d3e3cbe9c61a023ea7072038b
fatimazohra50 <haninfatimazohra@gmail.com>
2023-06-28 15:56:07 -0700

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,30 @@
 #include "main.h"
-
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * Return: int
+ */
+int sum_them_all(int n, int s);
 int _sqrt_recursion(int n)
 {
-	    return (sum_them_all(n ,1));
+	return (sum_them_all(n, 1));
 }
+/**
+ * sum_them_all - find square root
+ * @n: int
+ * @s: square root
+ * Return: int
+ */
 int sum_them_all(int n, int s)
 {
-	    if (s * s == n)
-		        {
-				    return s;
-				        }
-	        if (s * s < n)
-			    {
-				        return (sum_them_all(n, s + 1));
-
-					    }
-		    else 
-			        return -1;
-		        
+	if (s * s == n)
+	{
+		return (s);
+	}
+	if (s * s < n)
+	{
+		return (sum_them_all(n, s + 1));
+	}
+	else
+		return (-1);
 }
