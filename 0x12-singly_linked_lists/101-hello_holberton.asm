798a0ae05d5eaa756df48af2a2275f907ebdb74f
fatimazohra50 <haninfatimazohra@gmail.com>
2023-07-02 20:44:18 -0700

    m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,9 +2,10 @@ section      .text
     extern  printf
     global  main
 main:
-    mov      edi, msg
-    mov      eax, 0
-         printf
+    mov             edi, msg
+    mov             eax, 0
+    call     printf
+
 section    .data
-  msg db  'Hello, Holberton', 0xa, 0
+      msg db  'Hello, Holberton', 0xa, 0
 
