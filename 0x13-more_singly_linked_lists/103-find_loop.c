19b95f00c7e955ca78126e0b6bfadec50261f216
fatimazohra50 <haninfatimazohra@gmail.com>
2023-07-04 17:52:40 -0700

    n

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -17,7 +17,7 @@ listint_t *find_listint_loop(listint_t *head)
 	{
 		if (end == end->next)
 			return (end);
-		for (ptr = head; ptr != end; ptr =ptr->next)
+		for (ptr = head; ptr != end; ptr = ptr->next)
 			if (ptr == end->next)
 				return (end->next);
 	}
