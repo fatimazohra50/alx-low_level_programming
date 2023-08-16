a2af4cd93def701049d9db050a128eb58677080c
fatimazohra50 <haninfatimazohra@gmail.com>
2023-08-16 16:00:04 -0700

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the string name
+ * @f: the printing function pointer
+ * Return: Void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
