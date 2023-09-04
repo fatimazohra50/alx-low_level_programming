65d544a372c63ca93416cfc2bab1f42ad594c7fc
fatimazohra50 <haninfatimazohra@gmail.com>
2023-09-04 14:03:01 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,11 +9,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf[READ_BUF_SIZR * 8];
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	fd = open(filename, O_RDONL Y);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
